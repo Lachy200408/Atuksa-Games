3346766f666a300c60aab27b684a3b54a8750dcf
Lazaro <lachy200408@gmail.com>
2025-02-20 15:37:18 -0500

    mod: Restyling the badges

--- components/badge/Badge.tsx
+++ components/badge/Badge.tsx
@@ -9,7 +9,7 @@ export default function Badge({ children, className = '' }: Props) {
 	return (
 		<span
 			className={cn(
-				'flex items-center gap-2 rounded-full bg-primary-600/10 px-4 py-2 border border-primary-600/25 backdrop-blur-lg',
+				'flex items-center gap-2 rounded-full bg-primary-200/50 px-4 py-2 border border-primary-600/25 backdrop-blur-lg',
 				className,
 			)}
 		>
