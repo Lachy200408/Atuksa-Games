ce4c9b9d2b2d4228d7fc033686c309163f3e40a1
Lazaro <lachy200408@gmail.com>
2025-02-20 16:48:47 -0500

    mod: Restyling the pagination component

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import { cn } from '@/lib/utils/cn';
 
 interface Props {
@@ -18,20 +17,33 @@ export default function Pagination({
 		+index <= 2
 			? arrayPageIndex.slice(0, 3)
 			: +index === length
-			? arrayPageIndex.slice(index - 3)
-			: arrayPageIndex.slice(index - 2, index + 1);
-	const prevIndex = +index === 1 ? 1 : +index - 1;
-	const nextIndex = +index === length ? length : +index + 1;
+			? arrayPageIndex.slice(+index - 3)
+			: arrayPageIndex.slice(+index - 2, +index + 1);
 
 	return (
 		<div className="sticky bottom-16 mx-auto flex items-center justify-center gap-4">
-			<Link
-				href={urlCallback(prevIndex)}
-				className="brightness-100 md:brightness-90 md:hover:brightness-150 active:brightness-90 transition duration-300 rounded-full grid place-items-center aspect-square w-10 bg-primary-600/5 border border-primary-600/25 backdrop-blur-lg"
-			>
-				<ArrowLeftIcon className="size-4" />
-			</Link>
-			<ul className="flex items-center justify-center gap-4 p-2 bg-primary-600/5 border border-primary-600/25 backdrop-blur-lg rounded-full w-max">
+			<ul className="flex items-center justify-center gap-4 p-2 bg-primary-200/50 border border-primary-600/25 backdrop-blur-lg rounded-full w-max">
+				{+index >= length - 1 && (
+					<>
+						<li>
+							<Link
+								href={urlCallback(1)}
+								className={cn(
+									'brightness-100 md:brightness-90 md:hover:brightness-150 active:brightness-90 transition duration-300 rounded-full grid place-items-center aspect-square w-10',
+									1 === +index &&
+										'bg-primary-600/5 border border-primary-600/25 backdrop-blur-lg',
+								)}
+							>
+								1
+							</Link>
+						</li>
+						<li>
+							<div className="brightness-100 rounded-full grid place-items-center aspect-square w-10 bg-primary-600/5 border border-primary-600/25 backdrop-blur-lg">
+								...
+							</div>
+						</li>
+					</>
+				)}
 				{displayableArray.map((pageIndex) => (
 					<li key={pageIndex}>
 						<Link
@@ -46,13 +58,28 @@ export default function Pagination({
 						</Link>
 					</li>
 				))}
+				{+index < length - 1 && (
+					<>
+						<li>
+							<div className="brightness-100 rounded-full grid place-items-center aspect-square w-10 bg-primary-600/5 border border-primary-600/25 backdrop-blur-lg">
+								...
+							</div>
+						</li>
+						<li>
+							<Link
+								href={urlCallback(length)}
+								className={cn(
+									'brightness-100 md:brightness-90 md:hover:brightness-150 active:brightness-90 transition duration-300 rounded-full grid place-items-center aspect-square w-10',
+									length === +index &&
+										'bg-primary-600/5 border border-primary-600/25 backdrop-blur-lg',
+								)}
+							>
+								{length}
+							</Link>
+						</li>
+					</>
+				)}
 			</ul>
-			<Link
-				href={urlCallback(nextIndex)}
-				className="brightness-100 md:brightness-90 md:hover:brightness-150 active:brightness-90 transition duration-300 rounded-full grid place-items-center aspect-square w-10 bg-primary-600/5 border border-primary-600/25 backdrop-blur-lg"
-			>
-				<ArrowRightIcon className="size-4" />
-			</Link>
 		</div>
 	);
 }
